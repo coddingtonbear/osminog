595e407d77c51b8e9ab57bf96c8fbae192d23e4b
Adam Coddington <me@adamcoddington.net>
2017-08-07 16:24:53 -0700

    Adding initial version of Osminog.

---
+++ plugin/osminog/__init__.py
@@ -0,0 +1,70 @@
+import time
+
+import serial
+
+import octoprint.plugin
+from octoprint.events import Events
+
+
+class OsminogPlugin(
+    octoprint.plugin.StartupPlugin,
+    octoprint.plugin.EventHandlerPlugin,
+    octoprint.plugin.TemplatePlugin,
+    octoprint.plugin.SettingsPlugin,
+):
+    CHECK_INTERVAL = 10
+
+    def initialize(self):
+        self._last_filament_check = 0
+        self._printer_paused = False
+
+        port = self._settings.get(["port"])
+        self._osminog_port = None
+        if port:
+            self._osminog_port = serial.Serial(self._settings.get(["port"]))
+
+    def send_command(self, command):
+        if not self._osminog_port:
+            return
+
+        self._osminog_port.write(command.encode('utf8'))
+        return self._osminog_port.readline().strip()
+
+    def on_event(self, event, payload):
+        if event == Events.PRINT_STARTED:
+            self.send_command('POWERON')
+        if event in (
+            Events.PRINT_STOPPED,
+            Events.PRINT_FAILED,
+            Events.PRINT_CANCELLED,
+        ):
+            self.send_command('POWEROFF')
+
+        if time.time() > self._last_filament_check + self.CHECK_INTERVAL:
+            self._do_filament_check()
+
+    def _do_filament_check(self):
+        available = self.send_command('FILAMENT')
+        if available == '0':
+            if not self._printer_paused:
+                self._printer.pause_print()
+                self._printer_paused = True
+            self.send_command("BUZZER")
+
+        self._last_filament_check = time.time()
+
+    def get_template_configs(self):
+        return [
+            {
+                'type': 'sidebar',
+                'custom_bindings': False,
+            },
+            {
+                'type': 'settings',
+                'custom_bindings': False,
+            }
+        ]
+
+
+__plugin_name__ = 'Osminog'
+__plugin_implementation__ = OsminogPlugin()
---
+++ plugin/setup.py
@@ -0,0 +1,33 @@
+import octoprint_setuptools
+
+plugin_identifier = 'osminog'
+plugin_package = 'octoprint_osminog'
+plugin_name = 'Osminog'
+plugin_version = '0.1.0'
+plugin_description = 'Control your printer with Osminog.'
+plugin_author = 'Adam Coddington'
+plugin_author_email = 'me@adamcoddington.net'
+plugin_url = 'https://github.com/coddingtonbear/osminog'
+plugin_license = 'MIT'
+plugin_requires = []
+
+plugin_additional_data = []
+plugin_additional_packages = []
+plugin_ignored_packages = []
+
+
+setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
+    identifier=plugin_identifier,
+    package=plugin_package,
+    name=plugin_name,
+    version=plugin_version,
+    description=plugin_description,
+    author=plugin_author,
+    mail=plugin_author_email,
+    url=plugin_url,
+    license=plugin_license,
+    requires=plugin_requires,
+    additional_packages=plugin_additional_packages,
+    ignored_packages=plugin_ignored_packages,
+    additional_data=plugin_additional_data
+)
